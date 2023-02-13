6094470a8584c2e844d4b41e41bed585f9ecb051
JIWON KIM <kkjim1324@naver.com>
2023-02-13 19:23:05 +0900

    Initial commit

---
+++ src/main/java/ec/study/ecstudy/EcStudyApplication.java
@@ -0,0 +1,13 @@
+package ec.study.ecstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcStudyApplication.class, args);
+    }
+
+}
