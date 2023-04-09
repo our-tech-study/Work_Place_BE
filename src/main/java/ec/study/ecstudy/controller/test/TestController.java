3f17af2b6d3451d6024718125510bd84f5ed1e78
JIWON KIM <kkjim1324@naver.com>
2023-04-09 23:04:25 +0900

    feat : add swagger

--- src/main/java/ec/study/ecstudy/controller/test/TestController.java
+++ src/main/java/ec/study/ecstudy/controller/test/TestController.java
@@ -1,5 +1,6 @@
 package ec.study.ecstudy.controller.test;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/test")
+@ApiOperation(value = "테스트 컨트롤러" , notes = "서버 정상동작 확인을 위한 테스트 컨트롤러")
 public class TestController {
 
     @GetMapping("/hello")
