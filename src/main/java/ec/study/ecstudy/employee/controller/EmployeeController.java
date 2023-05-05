577a8e697b63aa4055f487105561b9a42d2e0b90
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2023-05-05 14:23:10 +0900

    feat : employee 레포지토리 추가

--- src/main/java/ec/study/ecstudy/employee/controller/EmployeeController.java
+++ src/main/java/ec/study/ecstudy/employee/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package ec.study.ecstudy.employee.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,22 @@ public class EmployeeController {
 
     @GetMapping("/all")
     public ResponseEntity<String> all(){
-        ResponseEntity<String> response = ResponseEntity.ok("ASD");
+        ResponseEntity<String> response = ResponseEntity.ok("result");
+
+        return response;
+    }
+
+    @GetMapping("/{team}")
+    public ResponseEntity<String> byTeam(@PathVariable String team){
+        ResponseEntity<String> response = ResponseEntity.ok("result");
+
+        return response;
+    }
+
+    @GetMapping("/profile/{empNo}")
+    @ApiOperation(value = "마이 프로필" , notes = "자신의 프로필 호출하는 API")
+    public ResponseEntity<String> profile(@PathVariable String empNo){
+        ResponseEntity<String> response = ResponseEntity.ok("result");
 
         return response;
     }
